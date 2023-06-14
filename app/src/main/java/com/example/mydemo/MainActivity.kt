bf50f0831294b38fb7ee19260b90f4f5439bc88f
AtulSinghSk <atul@shopkirana.com>
2023-06-14 18:39:28 +0530

    Change Commit

--- app/src/main/java/com/example/mydemo/MainActivity.kt
+++ app/src/main/java/com/example/mydemo/MainActivity.kt
@@ -8,7 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         FirstActivity.ShowToast(this,"Hello First Time Call")
 
        // MainDirectUdharActivity.SimpleToast(this,"Hello Bhagwan")
